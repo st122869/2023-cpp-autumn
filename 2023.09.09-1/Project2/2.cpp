9bc7713e9ec2ed594d01dfd3c60c779906704d27
st122869 <st122869@student.sbpu.ru>
2023-09-15 03:00:03 +0300

    Lesson 1 code update

--- 2023.09.09-1/Project2/2.cpp
+++ 2023.09.09-1/Project2/2.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 int main(int argc, char* argv[]) {
-	int N = 0;
+	int n = 0;
 	int a = 0;
-	std::cin >> N;
-	a = abs(N - 1) + 1;
-	std::cout << (1 + N) * a / 2;
+	std::cin >> n;
+	a = abs(n - 1) + 1;
+	std::cout << (1 + n) * a / 2;
 	return EXIT_SUCCESS;
 }
--- 2023.09.09-1/Project5/5.cpp
+++ 2023.09.09-1/Project5/5.cpp
@@ -4,7 +4,7 @@ int main(int argc, char* argv[]) {
 	int t = 0;
 	int a = 0;
 	std::cin >> v >> t;
-	a = ((v*t%109) + 109) % 109 + 1;
+	a = ((v * t % 109) + 109) % 109 + 1;
 	std::cout << a;
 	return EXIT_SUCCESS;
 }
