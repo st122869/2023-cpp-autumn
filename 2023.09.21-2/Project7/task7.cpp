2e38dd73e468622e2eec7c174c39d463cc7dcebf
st122869 <st122869@student.sbpu.ru>
2023-10-09 21:14:34 +0300

    Lesson 2 code update

--- 2023.09.21-2/Project7/task7.cpp
+++ 2023.09.21-2/Project7/task7.cpp
@@ -2,50 +2,68 @@
 int main(int argc, char* argv[]) {
 	int x = 0;
 	std::cin >> x;
-    if (x == 100) {
-        std::cout << "C";
-    }
-    else {
-        switch (x / 10) {
-        case 1: std::cout << "X";
-            break;
-        case 2: std::cout << "XX";
-            break;
-        case 3: std::cout << "XXX";
-            break;
-        case 4: std::cout << "XL";
-            break;
-        case 5: std::cout << "L";
-            break;
-        case 6: std::cout << "LX";
-            break;
-        case 7: std::cout << "LXX";
-            break;
-        case 8: std::cout << "LXXX";
-            break;
-        case 9: std::cout << "XC";
-            break;
-        }
-        switch (x % 10) {
-        case 1: std::cout << "I";
-            break;
-        case 2: std::cout << "II";
-            break;
-        case 3: std::cout << "III";
-            break;
-        case 4: std::cout << "IV";
-            break;
-        case 5: std::cout << "V";
-            break;
-        case 6: std::cout << "VI";
-            break;
-        case 7: std::cout << "VII";
-            break;
-        case 8: std::cout << "VIII";
-            break;
-        case 9: std::cout << "IX";
-            break;
-        }
-    }
-        return EXIT_SUCCESS;
+	if (x == 100) {
+		std::cout << "C";
+	}
+	else {
+		switch (x / 10) {
+		case 1:
+			std::cout << "X";
+			break;
+		case 2:
+			std::cout << "XX";
+			break;
+		case 3:
+			std::cout << "XXX";
+			break;
+		case 4:
+			std::cout << "XL";
+			break;
+		case 5:
+			std::cout << "L";
+			break;
+		case 6:
+			std::cout << "LX";
+			break;
+		case 7:
+			std::cout << "LXX";
+			break;
+		case 8:
+			std::cout << "LXXX";
+			break;
+		case 9:
+			std::cout << "XC";
+			break;
+		}
+		switch (x % 10) {
+		case 1:
+			std::cout << "I";
+			break;
+		case 2:
+			std::cout << "II";
+			break;
+		case 3:
+			std::cout << "III";
+			break;
+		case 4:
+			std::cout << "IV";
+			break;
+		case 5:
+			std::cout << "V";
+			break;
+		case 6:
+			std::cout << "VI";
+			break;
+		case 7:
+			std::cout << "VII";
+			break;
+		case 8:
+			std::cout << "VIII";
+			break;
+		case 9:
+			std::cout << "IX";
+			break;
+		}
+	}
+	return EXIT_SUCCESS;
 }
